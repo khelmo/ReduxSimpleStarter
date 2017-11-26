d007dec0c3851966b27d3085a82c8aabfbdcc1bc
Khelmo Freitas <Khelmo Freitas>
2017-11-26 21:05:43 -0200

    Add existing file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAtWzEkZ0CNh8hqiWe5Ft87vgP5gD75jUQ';
+
 
 //downwards data flow, most parent component responsible for fetching data
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
-const App = () => {
-  return (<div>
-            <SearchBar/>
-          </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        const API_KEY = 'AIzaSyAtWzEkZ0CNh8hqiWe5Ft87vgP5gD75jUQ';
+        super(props);
+        this.state = { videos:[] };
+        YTSearch({key: API_KEY, term: 'snowboard khelmo'}, (videos) => {
+            //console.log(data);
+            //this.state = { videos: data };
+            //this.state = { videos: videos };
+            //es6 syntax, key eh o mesmo nome do valor
+            this.setState({ videos });
+
+        });
+
+    }
+    render()
+    {
+        return (<div>
+                    <SearchBar />
+                    <VideoDetail video={this.state.videos[0]} />
+                    <VideoList videos={this.state.videos} />
+                </div>
+        );
+    }
 }
 
 
