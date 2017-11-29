5d5cc2c4cf8df0817bbff121430cc3002a96c48a
khelmo <khelmo@gmail.com>
2017-11-29 15:49:46 -0200

    commit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,16 +4,21 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { term: 'Start'};
+        this.state = { term: ''};
     }
 
     render() {
         //controlled component
         return(
             <div className="search-bar"> <input value={this.state.term}
-            onChange={ e => this.setState({term: e.target.value}) } />
+            onChange={ e => this.onInputChange(e.target.value) } />
         </div>);
 
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 
 };
